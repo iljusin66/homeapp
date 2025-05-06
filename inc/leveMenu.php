81068b5e4286bcaebd3c79f62626d5dbde00f217
Ivan Latečka <ivan.latecka@gmail.com>
2025-05-06 16:55:02 +0200

    Úprava navigace

--- inc/leveMenu.php
+++ inc/leveMenu.php
@@ -1,18 +1,14 @@
 <div class="col-2 d-print-none" id="leve-menu">
     <div class="row pe-2">
-        <ul class="nav flex-column">
-            <li class="nav-item"><a class="nav-link" href="ciselnikZamestnancu.php">Seznam zaměstnanců</a></li>
-        </ul>
-
         <h6>Nadpis</h6>
-        <ul class="nav flex-column mb-5">
-            <li class="nav-item"><a class="nav-link <?= (c_ScriptBaseName == 'zamestnanecEdit') ? 'active' : ''?>" href="zamestnanecEdit.php?idzamestnance=<?= $oZam->idzamestnance ?>">Konfigurace</a></li>
-            <li class="nav-item"><a class="nav-link <?= (c_ScriptBaseName == 'vypocetOdmen') ? 'active' : ''?>" href="vypocetOdmen.php?idzamestnance=<?= $oZam->idzamestnance ?>">Výpočet odměny</a></li>
-            <li class="nav-item"><a class="nav-link <?= (c_ScriptBaseName == 'zamestnanecOdmena') ? 'active' : ''?>" href="zamestnanecOdmena.php?idzamestnance=<?= $oZam->idzamestnance ?>&last=1">Poslední odměna</a></li>
-            <li class="nav-item"><a class="nav-link <?= (c_ScriptBaseName == 'zamestnanecOdmeny') ? 'active' : ''?>" href="zamestnanecOdmeny.php?idzamestnance=<?= $oZam->idzamestnance ?>">Přehled odměn</a></li>
+        <ul class="nav flex-column mb-1">
+            <li class="nav-item"><a class="nav-link <?= (c_ScriptBaseName == 'vlozitOdpocet') ? 'active' : ''?>" href="<?= c_MainUrl; ?>vlozitOdpocet.php?idz=1"><?= __('Nový odečet') ?></a></li>
         </ul>
         <ul class="nav flex-column">
-            <li class="nav-item"><a class="nav-link <?= (c_ScriptBaseName == 'exportOdmen') ? 'active' : ''?>" href="exportOdmen.php">Přehled odměn</a></li>
+            <li class="nav-item"><a class="nav-link <?= (c_ScriptBaseName == 'nastaveniZarizeni') ? 'active' : ''?>" href="<?= c_MainUrl; ?>nastaveniZarizeni.php?idz=1"><?= __('Nastavení') ?></a></li>
+            <li class="nav-item"><a class="nav-link <?= (c_ScriptBaseName == 'logout') ? 'active' : ''?>" href="<?= c_MainUrl; ?>logout.php"><?= __('Odhlásit') ?></a></li>
         </ul>
     </div>
+
+    <div class="text-center"></div>
 </div>
