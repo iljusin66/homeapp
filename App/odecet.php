8a438947e19907fa8439c07440b7dba30e677609
Ivan Latečka <ivan.latecka@gmail.com>
2025-05-07 19:29:01 +0200

    První nástřel seznamu zadaných odpočtů

--- App/odecet.php
+++ App/odecet.php
@@ -17,8 +17,11 @@ new config();
 class odecet extends zarizeni{
 
     private static $initialized = false;
+    private $aUser = [];
+    private $aOdecty = [];
 
-    function __construct() {
+    function __construct($aUser = []) {
+        $this->aUser = $aUser;
         if (!self::$initialized) {
             parent::__construct();
             self::$initialized = true;
@@ -26,5 +29,21 @@ class odecet extends zarizeni{
         
     }
 
-            
+    private function nactiOdecty() {
+        $q = 'SELECT o.*, z.nazev, r.role, mj.jednotka, ui.username AS userZadal, uu.username AS userOpravil FROM odecet_zarizeni AS o
+            JOIN zarizeni AS z ON z.id = o.idzarizeni AND z.id = ?
+            JOIN zarizeni2users AS zu ON z.id = zu.idzarizeni AND zu.iduser = ?
+            JOIN role AS r ON r.id = zu.idrole
+            JOIN cis_merne_jednotky AS mj ON mj.id = z.idjednotky
+            JOIN users AS ui ON ui.id = o.zadal
+            LEFT JOIN users AS uu ON uu.id = o.opravil';
+        debug([$q, [$this->aZarizeni['id'], $this->aUser['id']]]); 
+            $rows = db::fa($q, [$this->aZarizeni['id'], $this->aUser['id']]);
+    }
+
+    public function nactiSeznamOdectu() {
+        $this->nactiOdecty();
+        if (empty($this->aOdecty)) return false;
+        return $this->aOdecty;
+    }
 }
---
+++ seznamOdectu.php
@@ -0,0 +1,51 @@
+<?php
+USE Latecka\Utils\utils;
+
+require_once 'autoload.php';
+
+$oUser = new user();
+$oOdecet = new odecet($oUser->aUser);
+$oOdecet->nactiSeznamOdectu();
+?><!DOCTYPE html>
+<html lang="cs">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="<?= c_MainUrl; ?>Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- https://icons.getbootstrap.com/ -->
+    <link href="<?= c_MainUrl; ?>Bootstrap/css/icons/bootstrap-icons.css" rel="stylesheet">
+    <title><?= $oOdecet->aZarizeni['nazev'] ?>: <?= ($oOdecet->aOdecet["id"]==0) ? __('vložit odečet') : __('oprava odečtu') ?></title>
+    <script src="<?= c_MainUrl; ?>inc/jquery-3.6.4.min.js"></script>
+    <script src="<?= c_MainUrl; ?>Bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= c_MainUrl; ?>inc/home.js?ch=<?= md5_file('inc/home.js') ?>"></script>
+    <link href="<?= c_MainUrl; ?>inc/css/home.css?ch=<?= md5_file(c_FileRoot.'inc/home.css') ?>" rel="stylesheet">
+    <style>
+
+    </style>
+  </head>
+  <body class="ps-3 pe-3 m-0 border-0 bg-light">
+        <?php include('inc/navbar-top.php') ?>
+        <div class="row">
+            <div class="col p-3 bg-white m-2 rounded-3">
+                <div class="row">
+                    <div class="col-2 d-print-none d-none d-md-block"></div>
+                    <div class="col ps-0">
+                        <h1 class="fs-3 ps-0"><?= $oOdecet->aZarizeni['nazev'] ?></h1>
+                    </div>
+                </div>
+                <div class="row pt-0">
+                    <?php include_once 'inc/leveMenu.php'; ?>
+                    <div class="col" style="min-height: 85vh">
+                        <div id="dataContainer" class="row">
+                            
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+        <?php include_once 'inc/modalInfo.php'; ?>
+        <script src="<?= c_MainUrl; ?>inc/validation-form.js"></script>
+    </body>
+</html>
