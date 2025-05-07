29bf131fd6a59d3b7974d3c57fd0a0eea0244820
Ivan Latečka <ivan.latecka@gmail.com>
2025-05-07 07:23:26 +0200

    Oprava leveho menu

--- inc/leveMenu.php
+++ inc/leveMenu.php
@@ -2,10 +2,10 @@
     <div class="row pe-2">
         <h6>Nadpis</h6>
         <ul class="nav flex-column mb-1">
-            <li class="nav-item"><a class="nav-link <?= (c_ScriptBaseName == 'zapisOdecet') ? 'active' : ''?>" href="<?= c_MainUrl; ?>vlozitOdecet.php?idz=1"><?= __('Nový odečet') ?></a></li>
+            <li class="nav-item"><a class="nav-link <?= (c_ScriptBaseName == 'zapisOdecet') ? 'active' : ''?>" href="<?= c_MainUrl; ?>zapisOdecet.php?idz=1"><?= __('Nový odečet') ?></a></li>
         </ul>
         <ul class="nav flex-column">
-            <li class="nav-item"><a class="nav-link <?= (c_ScriptBaseName == 'nastaveniZarizeni') ? 'active' : ''?>" href="<?= c_MainUrl; ?>nastaveniZarizeni.php?idz=1"><?= __('Nastavení') ?></a></li>
+            <li class="nav-item"><a class="nav-link <?= (c_ScriptBaseName == 'zapisZarizeni') ? 'active' : ''?>" href="<?= c_MainUrl; ?>zapisZarizeni.php?idz=1"><?= __('Nastavení') ?></a></li>
             <li class="nav-item"><a class="nav-link <?= (c_ScriptBaseName == 'logout') ? 'active' : ''?>" href="<?= c_MainUrl; ?>logout.php"><?= __('Odhlásit') ?></a></li>
         </ul>
     </div>
